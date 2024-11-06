0f0c89dab5ba6b26c75d3f628cecb321da6106bc
Kotsiuruba Maksim <hessyqt@mail.ru>
2024-11-06 22:05:00 +0300

    first

--- src/main/java/ru/kata/spring/boot_security/demo/beans/controllers/UsersController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/beans/controllers/UsersController.java
@@ -24,26 +24,21 @@ public class UsersController {
         this.roleService = roleService;
     }
 
-    @GetMapping("/admin")
-    public String getUsers(Model model) {
-        model.addAttribute("users", userService.getAllUsers());
-        return "/admin/show";
-    }
 
-    @GetMapping("/admin/addUser")
-    public String showUserAddPage(Model model) {
-        model.addAttribute("user", new User());
+    @GetMapping("/admin")
+    public String getUsers(Model model,Principal principal) {
+        model.addAttribute("users", userService.getAllUsers());
+        model.addAttribute("user",userService.findByEmail(principal.getName()));
+        model.addAttribute("userForForm", new User());
         model.addAttribute("roles", roleService.findAll());
-        return "/admin/addUser";
+        return "/admin/show";
     }
 
     @GetMapping("/user")
     public String showUserForm(Model model, Principal principal) {
-        User foundUser = userService.findByEmail(principal.getName());
-        model.addAttribute("user", foundUser);
+        model.addAttribute("user", userService.findByEmail(principal.getName()));
         return "/users/userPage";
     }
-
 
     @PostMapping("/admin/addNewUser")
     public String addUser(@ModelAttribute User user) {
@@ -51,17 +46,9 @@ public class UsersController {
         return "redirect:/admin";
     }
 
-    @PostMapping("/admin/editUserPage")
-    public String showUserEditPage(@RequestParam("id") int id, Model model) {
-        User foundUser = userService.findById(id);
-        model.addAttribute("user", foundUser);
-        model.addAttribute("roles", roleService.findAll());
-        return "/admin/editUserPage";
-    }
-
     @PostMapping("/admin/editUser")
-    public String editUser(@ModelAttribute User user, @RequestParam Set<Role> roles) {
-        userService.updateUser(user.getId(), user, roles);
+    public String editUser(@ModelAttribute User user, @RequestParam(required = false) Set<Role> roles, @RequestParam(name = "newPassword") String newPassword) {
+        userService.updateUser(user, roles,newPassword);
         return "redirect:/admin";
     }
 
